85bb5ad118347401f7d17dec062f75c741a3da5f
Daniel T. Thorpe <daniel.thorpe@citrix.com>
2022-02-22 17:34:29 +0000

    fix(oled.c): rewrite OLED code to respect new layers

--- keyboards/crkbd/keymaps/dantho281/oled.c
+++ keyboards/crkbd/keymaps/dantho281/oled.c
@@ -6,29 +6,28 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
-
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
-            break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
-            break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
+    switch (get_highest_layer(layer_state)) {
+    case _DVORAK:
+        oled_write_P(PSTR("Base\n"), false);
+        break;
+    case _RAISE:
+        oled_write_P(PSTR("Raise"), false);
+        break;
+    case _LOWER:
+        oled_write_P(PSTR("Lower"), false);
+        break;
+    case _ADJUST:
+        oled_write_P(PSTR("Adj\n"), false);
+        break;
+    default:
+        oled_write_ln_P(PSTR("Undef"), false);
+    }
+    if (swap_space_enter) {
+        oled_write_ln_P(PSTR("Game"), false);
+    } else {
+        oled_write_ln_P(PSTR("\n"), false);
     }
 }
 
